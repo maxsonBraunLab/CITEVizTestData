273b2336fbc78e48ae65a3d5b15041be0a43c866
gartician <kongga2017@gmail.com>
2022-10-13 15:08:25 -0700

    include R/ folder

---
+++ R/CITEVizTestData.R
@@ -0,0 +1,49 @@
+#' @title Download a PBMC CITE-Seq data for CITEViz in 
+#' Seurat/SingleCellExperiment format using ExperimentHub
+#' 
+#' @description This function helps download a CITE-Seq dataset from 
+#' Hao. et. al. 2021 for use with CITEViz. The dataset contains 
+#' 2500 cells and 228 antibodies.
+#'
+#' @param format Either "Seurat" or "SingleCellExperiment"
+#' @return A Seurat [default] or SingleCellExperiment object.
+#' @examples \donttest{
+#' file_1 = CITEVizTestData(format = "Seurat")
+#' file_2 = CITEVizTestData(format = "SingleCellExperiment")
+#' }
+#' 
+#' @importFrom ExperimentHub ExperimentHub
+#' @importFrom AnnotationHub query
+#' 
+#' @export
+CITEVizTestData <- function(format = "Seurat") {
+  
+  all_datasets <- c("CITEVizTestDataSeurat",
+                    "CITEVizTestDataSingleCellExperiment")
+  all_formats <- c("Seurat", "SingleCellExperiment")
+  
+  if (format %in% all_formats) {
+  
+    eh = ExperimentHub::ExperimentHub()
+    pkgname <- "CITEVizTestData"
+    q <- AnnotationHub::query(eh, pkgname)
+    
+    if (format == "Seurat") {
+      # I know I am not supposed to use the index, 
+      # but I currently don't have an EHID right now.
+      out <- q[[1]]
+    }
+    
+    if (format == "SingleCellExperiment")
+      out <- q[[2]]
+  
+  return(out)
+    
+  } else {
+    
+    message("Input format argument could not be found.")
+    stop("Available formats: ['Seurat', 'SingleCellExperiment']")
+  
+  }
+}
+
